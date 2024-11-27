bd177813a2018872325df248f6dd28e936820437
Prashant Kadam <prashantkadam@Prashants-MacBook-Air.local>
2024-11-27 13:52:31 +0530

    start with matplot

---
+++ matplot_learn.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 27 13:49:37 2024
+
+@author: prashantkadam
+"""
+
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.title("Example Plot")
+plt.show()
